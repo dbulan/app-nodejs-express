02ba2ec5ba9ba6df56378a59c2a4dda58afbd700
dbulan <dimitar.bulanov@gmail.com>
2018-08-22 23:58:54 +0300

    send json to view

--- 001-simple-structure/app/routes.js
+++ 001-simple-structure/app/routes.js
@@ -13,7 +13,13 @@ router.get('/', function(req, res) {
 });
 
 router.get('/about', function(req, res) {
-  res.render('pages/about');
+  var users = [
+    { name: 'Holly', email: 'holly@scotch.io', avatar: 'http://placekitten.com/300/300'},
+    { name: 'Chris', email: 'chris@scotch.io', avatar: 'http://placekitten.com/400/400'},
+    { name: 'Ado', email: 'Ado@scotch.io', avatar: 'http://placekitten.com/500/500'},
+    { name: 'Samantha', email: 'Samantha@scotch.io', avatar: 'http://placekitten.com/700/700'}
+  ];
+  res.render('pages/about', { users: users });
 });
 
 router.get('/contact', function(req, res) {
