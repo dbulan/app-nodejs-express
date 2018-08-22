e197a03219adbe4ec57a851c4f6c4025bf1e3735
dbulan <dimitar.bulanov@gmail.com>
2018-08-22 23:53:37 +0300

    ejs template displaying

--- 001-simple-structure/app/routes.js
+++ 001-simple-structure/app/routes.js
@@ -8,8 +8,17 @@ module.exports = router;
 // app.get change to router.app
 router.get('/', function(req, res) {
   //res.send('hello world!');
-  res.sendFile(path.join(__dirname, '../views/pages/index.html'));
+  //res.sendFile(path.join(__dirname, '../views/pages/index.html'));
+  res.render('pages/home');
 });
+
 router.get('/about', function(req, res) {
-  res.sendFile(path.join(__dirname, '../views/pages/about.html'));
+  res.render('pages/about');
+});
+
+router.get('/contact', function(req, res) {
+  res.render('pages/contact');
+});
+
+router.post('/contact', function(req, res) {
 });
--- 001-simple-structure/index.js
+++ 001-simple-structure/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var expressLayouts = require('express-ejs-layouts');
+
 var app = express();
 var port = 8080;
 
+// use ejs and express layots
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+// route our app
 var router = require('./app/routes');
 app.use('/', router);
+
+// set static files (css, images, etc.) location
+app.use(express.static(__dirname+'/public'));
 
 // start the server
 app.listen(port, function() {
